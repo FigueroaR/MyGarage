0768d1a7615f23506b273ca6bbb5198f9d0cecd4
your_name <raul.a.figueroa@hotmail.com>
2019-11-01 22:17:34 -0400

    vehicle obect added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -46,4 +46,12 @@ class UserController < ApplicationController
     get '/sessions/new/car' do 
         erb :'registrations/newcar'
     end
+
+    post '/new/car' do 
+        @car = Car.new(make: params["make"], model: params["model"], year: params["year"])
+        @car.save
+        session[:car_id] = @car.id
+        #puts params
+        redirect '/users/home'
+    end
 end 
